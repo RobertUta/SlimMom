e09b5bc67d5a787211c269325d8c1edc96367673
Uță Robert Constantin <roy_robert_99@yahoo.com>
2024-07-08 19:10:16 +0300

    pages
    pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
-export const App = () => {
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { lazy } from 'react';
+
+import Layout from 'components/Layout/Layout';
+
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchCurrentUser } from './redux/auth/AuthOperation';
+
+import PrivateRoute from './PrivateRoute';
+
+import useAuth from './hooks/UseAuth';
+import { Box, Container } from '@chakra-ui/react';
+import PublicRoute from './PublicRoute';
+
+const RegistrationForm = lazy(() => import('./pages/Registration'));
+const Contacts = lazy(() => import('./pages/Contacts'));
+const Login = lazy(() => import('./pages/Login'));
+
+function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCurrentUser());
+  }, [dispatch]);
+
+  const { isRefreshing } = useAuth();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    !isRefreshing && (
+      <Container maxW='1200px' centerContent>
+        <Box padding='4' width="100%">
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Navigate to='/contacts' replace />} />
+
+              <Route
+                path="/register"
+                element={
+                  <PublicRoute
+                    component={<RegistrationForm />}
+                    redirectTo="/contacts"
+                  />
+                }
+              />
+
+              <Route
+                path="/login"
+                element={
+                  <PublicRoute component={<Login />} redirectTo="/contacts" />
+                }
+              />
+
+              <Route
+                path="/contacts"
+                element={
+                  <PrivateRoute component={<Contacts />} redirectTo="/login" />
+                }
+              />
+
+              <Route path="*" element={<Contacts />} />
+            </Route>
+          </Routes>
+        </Box>
+
+      </Container >
+    )
   );
-};
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { persistor, store } from './components/redux/store';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { PersistGate } from 'redux-persist/integration/react';
+import { ChakraBaseProvider } from '@chakra-ui/react';
+import theme from './theme';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraBaseProvider theme={theme}>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
+            <App />
+          </BrowserRouter>
+        </ChakraBaseProvider>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
